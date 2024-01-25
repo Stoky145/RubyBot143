735b5bdd6fc584508c72eeeade35d52d47ad3d16
Simpl143 <86554143+Simpl143@users.noreply.github.com>
2024-01-26 00:49:33 +0300

    Initial commit

---
+++ src/main/java/org/example/rubybot143/RubyBot143Application.java
@@ -0,0 +1,13 @@
+package org.example.rubybot143;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RubyBot143Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RubyBot143Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/rubybot143/RubyBot143ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.rubybot143;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RubyBot143ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
