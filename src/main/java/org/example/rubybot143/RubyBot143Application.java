33cd897dac09937b01ea8ed31568b4716b09dc52
Simpl143 <86554143+Simpl143@users.noreply.github.com>
2024-02-11 15:43:35 +0300

    added entities to bd

--- src/main/java/org/example/rubybot143/RubyBot143Application.java
+++ src/main/java/org/example/rubybot143/RubyBot143Application.java
@@ -1,13 +1,23 @@
 package org.example.rubybot143;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
 public class RubyBot143Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RubyBot143Application.class, args);
-	}
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(RubyBot143Application.class);
+        UsersRepository repository = context.getBean(UsersRepository.class);
+
+        repository.save(new Users("Jeck", "Killed", "JeckKill@yandex.ru"));
+        repository.save(new Users("Bob", "Hegrod", "bob543@mail.ru"));
+        repository.save(new Users("Bill", "Novikov", "NovikBil@yandex.ru"));
+
+    }
 
 }
